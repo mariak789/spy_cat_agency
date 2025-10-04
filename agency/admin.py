fea74a38887ca113b9b572c27714e2a87fe05f57
Maria <marusjakarassmaria789@gmail.com>
2025-10-04 22:11:02 +0300

    register models in Django admin

--- agency/admin.py
+++ agency/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import SpyCat, Mission, Target
 
-# Register your models here.
+@admin.register(SpyCat)
+class SpyCatAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "breed", "years_of_experience", "salary")
+
+class TargetInline(admin.TabularInline):
+    model = Target
+    extra = 0
+
+@admin.register(Mission)
+class MissionAdmin(admin.ModelAdmin):
+    list_display = ("id", "cat", "complete", "created_at")
+    inlines = [TargetInline]
